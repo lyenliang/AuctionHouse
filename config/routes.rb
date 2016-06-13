53beddd6e48dad8ab808ecf24573691efb0c460e
Ben <lyenliang@gmail.com>
2016-06-13 09:11:08 +0000

    Generate order info

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :orders
+
   # You can have the root of your site routed with "root"
   root 'products#index'
 
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
 
   resources :products do
     member do
