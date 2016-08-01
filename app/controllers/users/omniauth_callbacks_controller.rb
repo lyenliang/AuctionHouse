b44fcdc1c409ee134e709da96a60c085824899ec
Ben <lyenliang@gmail.com>
2016-08-01 14:22:43 +0000

    Fix a typo

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksControlller
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
         
