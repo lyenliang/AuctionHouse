00bb693e381b8250d35e1bbe054c643f6c64fb88
Ben <lyenliang@gmail.com>
2016-10-15 15:31:51 +0000

    Products' name can't be too long

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ create_users = for i in 1..9 do
 end
 
 product = Product.new
-product.title = "【小野千尋】花朵印花蝴蝶結喇叭袖連身裙"
+product.title = "花朵印花蝴蝶結喇叭袖連身裙"
 product.price = "550"
 product.quantity = "50"
 product.description = ""
@@ -26,7 +26,7 @@ product.photo.save!
 
 
 product = Product.new
-product.title = "【Jessi Closet】時尚鬆緊腰修身長袖蕾絲 連身裙"
+product.title = "時尚鬆緊腰修身長袖蕾絲 連身裙"
 product.price = "790"
 product.quantity = "50"
 product.description = ""
@@ -36,7 +36,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic2.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【MOSS.Woman】圓領七分袖花瓣波浪裙襬修身洋裝"
+product.title = "圓領七分袖花瓣波浪裙襬修身洋裝"
 product.price = "799"
 product.quantity = "50"
 product.description = ""
@@ -46,7 +46,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic3.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【Jessi Closet】時尚修身格子長袖中長款包臀 連身裙"
+product.title = "時尚修身格子長袖中長款包臀 連身裙"
 product.price = "790"
 product.quantity = "50"
 product.description = ""
@@ -56,7 +56,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic4.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【Jessi Closet】時尚修身格子長袖中長款包臀 連身裙"
+product.title = "時尚修身格子長袖中長款包臀 連身裙"
 product.price = "790"
 product.quantity = "50"
 product.description = ""
@@ -66,7 +66,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic5.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【EOWYN】女韓版條紋長袖小清新淑女連衣裙"
+product.title = "女韓版條紋長袖小清新淑女連衣裙"
 product.price = "1100"
 product.quantity = "50"
 product.description = ""
@@ -76,7 +76,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic6.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【EOWYN】歐洲站長褲大碼連體褲兩件套裝"
+product.title = "歐洲站長褲大碼連體褲兩件套裝"
 product.price = "1250"
 product.quantity = "50"
 product.description = ""
@@ -86,7 +86,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic7.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【小野千尋】後挖洞圓領長袖T+網紗背心洋裝"
+product.title = "後挖洞圓領長袖T+網紗背心洋裝"
 product.price = "590"
 product.quantity = "50"
 product.description = ""
@@ -96,7 +96,7 @@ product.photo.image.store!(File.open(File.join(Rails.root, 'public/pic8.jpg')))
 product.photo.save!
 
 product = Product.new
-product.title = "【Jessi Closet】時尚氣質OL純色魚尾裙 兩件式套裝"
+product.title = "時尚氣質OL純色魚尾裙 兩件式套裝"
 product.price = "890"
 product.quantity = "50"
 product.description = ""
